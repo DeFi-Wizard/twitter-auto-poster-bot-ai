85e38f27eff5e649aecf607ace29ef50f1b43e7c
DeFi-Wizard <mattsadservices@gmail.com>
2024-10-28 12:44:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ async function run() {
   });
 
   // Write your prompt here
-  const prompt =
-    "generate content based around DeFi, crypto, pump fun, and more specifically memecoins, involving statistics, tips and tricks, news, something new or some rant or some advice as a tweet, it should not be vague and should be engaging and unique; under 280 characters and should be plain text, you can use hashtags and emojis";
+  const prompt = "generate content based around DeFi, crypto, pump.fun, and memecoins, involving statistics, tips and tricks, or news as a tweet, it should not be vague and should be engaging and unique; include a call to like & follow. make it under 280 characters and should be plain text, you can use hashtags and emojis";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
